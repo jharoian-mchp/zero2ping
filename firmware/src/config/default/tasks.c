aa979b2c39f33e333d39eb3c0fe586cb7180e4e7
John Haroian <john.haroian@microchip.com>
2023-06-21 14:30:36 -0700

    Opened Content Manager and regenerated Needed CMSIS-View fix:   C:\Projects\harmony_test\CMSIS-View>git checkout tag/tools/eventlist/1.1.0

--- firmware/src/config/default/tasks.c
+++ firmware/src/config/default/tasks.c
@@ -59,6 +59,16 @@
 // Section: RTOS "Tasks" Routine
 // *****************************************************************************
 // *****************************************************************************
+
+void _TCPIP_STACK_Task(  void *pvParameters  )
+{
+    while(1)
+    {
+        TCPIP_STACK_Task(sysObj.tcpip);
+        vTaskDelay(1 / portTICK_PERIOD_MS);
+    }
+}
+
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
 
@@ -67,16 +77,6 @@ void _APP_Tasks(  void *pvParameters  )
     while(1)
     {
         APP_Tasks();
-    }
-}
-
-
-void _TCPIP_STACK_Task(  void *pvParameters  )
-{
-    while(1)
-    {
-        TCPIP_STACK_Task(sysObj.tcpip);
-        vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
 
